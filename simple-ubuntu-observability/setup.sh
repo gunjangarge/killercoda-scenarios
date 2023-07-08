6c0fa37a7089d39a05c5c703c07d7a8d3ee3f540
Gunjan Garge <gunjangarge@gmail.com>
2023-07-08 22:20:38 +0530

    Update setup.sh

--- simple-ubuntu-observability/setup.sh
+++ simple-ubuntu-observability/setup.sh
@@ -30,7 +30,7 @@ datasources:
    url: http://172.30.1.2:9090
    version: 1
 EOF
-docker run -d --name=grafana -p 3000:3000 -v /tmp/setup/Prometheus-ds.yaml:/etc/grafana/provisioning/datasources/datasource.yml grafana/grafana
+docker run -d --name=grafana -p 3000:3000 -v /tmp/setup/datasource.yaml:/etc/grafana/provisioning/datasources/datasource.yml grafana/grafana
 docker run -d --name pushgw -p 9091:9091 prom/pushgateway
 docker run -d --name=prom -p 9090:9090 -v /tmp/setup/prometheus.yaml:/etc/prometheus/prometheus.yml prom/prometheus
 docker run --cap-add=SYS_TIME -d --net="host" --pid="host" -p 9100:9100 --name=nodeexporter -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter:latest
