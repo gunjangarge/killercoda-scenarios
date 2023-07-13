f8b300d295a6f9ae043b6c96f422dd49e012e85d
Gunjan Garge <gunjangarge@gmail.com>
2023-07-13 12:13:29 +0530

    Update setup.sh

--- simple-ubuntu-observability/setup.sh
+++ simple-ubuntu-observability/setup.sh
@@ -32,13 +32,26 @@ datasources:
 EOF
 
 wget https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml -P /tmp/setup
-sed -i -e 's/        - --metric-resolution=15s/        - --metric-resolution=15s\n        - --kubelet-insecure-tls/g' components.yaml
+sed -i -e 's/        - --metric-resolution=15s/        - --metric-resolution=15s\n        - --kubelet-insecure-tls/g' /tmp/setup/components.yaml
 kubectl apply -f /tmp/setup/components.yaml
 
 
 docker run -d --name=grafana -p 3000:3000 -v /tmp/setup/datasource.yaml:/etc/grafana/provisioning/datasources/datasource.yml grafana/grafana
 docker run -d --name pushgw -p 9091:9091 prom/pushgateway
-docker run -d --name=prom -p 9090:9090 -v /tmp/setup/prometheus.yaml:/etc/prometheus/prometheus.yml -v /tmp/setup/certs:/k8s/certs prom/prometheus
+docker run -d --name=prom -p 9090:9090 -v /tmp/setup/prometheus.yaml:/etc/prometheus/prometheus.yml prom/prometheus
 docker run --cap-add=SYS_TIME -d --net="host" --pid="host" -p 9100:9100 --name=nodeexporter -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter:latest
 
+# setup desktop
+apt update -y
+apt install -y xfce4 tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer novnc python3-websockify python3-numpy 
+cat <<EOF >~/.vnc/xstartup
+#!/bin/bash
+
+PATH=/usr/bin:/usr/sbin
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+exec startxfce4 &
+EOF
+sudo vncserver -localhost no -SecurityTypes None --I-KNOW-THIS-IS-INSECURE
+websockify -D --web=/usr/share/novnc 9999 localhost:5901
 echo "Done" > /tmp/setup/done.txt
