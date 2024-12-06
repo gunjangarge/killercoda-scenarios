cf92f6407e50cb4832a8b8136b7fc4528d974b0f
Gunjan Garge <gunjangarge@gmail.com>
2024-12-06 22:42:59 +0530

    Update setup.sh

--- localstack-terraform-ansible/setup.sh
+++ localstack-terraform-ansible/setup.sh
@@ -20,6 +20,7 @@ apt-get install terraform -y
 
 # install ansible
 python3 -m pip install --user ansible
+sudo apt install ansible
 
 # run localstack
 docker run -d --name=localstack -p 4566:4566 -p 4510-4559:4510-4559 localstack/localstack
