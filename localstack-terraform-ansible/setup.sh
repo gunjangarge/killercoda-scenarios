dd26d6feffbeed69e95bdf27451c77ebb93a5dc1
Gunjan Garge <gunjangarge@gmail.com>
2025-05-18 20:43:02 +0530

    Update setup.sh

--- localstack-terraform-ansible/setup.sh
+++ localstack-terraform-ansible/setup.sh
@@ -5,6 +5,18 @@ export DEBIAN_FRONTEND=noninteractive
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
+# firefox
+add-apt-repository ppa:mozillateam/ppa -y
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+
+Package: firefox
+Pin: version 1:1snap*
+Pin-Priority: -1
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
 # install misc 
 apt-get update -y
 apt-get install -y gnupg software-properties-common
