11d73b7f0e499a6c145495fd10590e51a49d51b9
Gunjan Garge <gunjangarge@gmail.com>
2025-05-18 13:49:20 +0530

    Update progress.sh

--- localstack-terraform-ansible/progress.sh
+++ localstack-terraform-ansible/progress.sh
@@ -1,1 +1,1 @@
-clear; echo -n Installing ; while ! test -f /tmp/setup/done.txt; do echo -n .; sleep 1; done; echo; echo INSTALL COMPLETED
+clear; echo -n `date` - Installing ; while ! test -f /tmp/setup/done.txt; do echo -n .; sleep 1; done; echo; echo `date` - Installation completed.
